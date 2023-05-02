f1ac5abf43a4e9a2dff5577676c3e10ee857d9cf
Brian J. Pinney <brianj.pinney@gmail.com>
2023-05-02 13:17:20 -0400

    Added auth token

--- functions/src/shows.js
+++ functions/src/shows.js
@@ -1,5 +1,7 @@
 import { FieldValue } from "firebase-admin/firestore";
+import jwt from "jsonwebtoken"
 import { db } from "./dbConnect.js";
+import { secretKey } from "../secrets.js";
 
 
 const coll = db.collection('shows')
@@ -11,8 +13,18 @@ export async function getShows(req ,res){
 }
 
 export async function addShow(req, res){
+    const token = req.headers.authorization
+    if (!token) {
+        res.status(401).send({message: "Unauthorized. A valide token is required."})
+        return
+    }
+    const decoded = jwt.verify(token, secretKey)
+    if (!decoded) {
+        res.status(401).send({message: "A valid token is required"})
+        return
+    }
     const {title, poster, seasons} = req.body
-    if(!title){
+    if(!title || !poster || !seasons){
         res.status(400).send({message: "Show title is required."})
         return
     }
